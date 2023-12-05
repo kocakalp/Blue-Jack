21a62dbb18b4bfc4219b366e661cba1bd7580885
Alp KOÇAK <alpqkocak@gmail.com>
2023-12-05 23:43:31 +0300

    computer gets cards
    now computer gets the first 4 card and main is changed to this

--- Main.java
+++ Main.java
@@ -3,18 +3,26 @@ public class Main {
 
         GameDeck game = new GameDeck();
 
-        String [] deck =game.shuffleDeck();
+        String [] deck =game.shuffleDeck();//kartarı oluşturu ve shuffle eder
 
-        PlayerDeckPlayer player = new PlayerDeckPlayer(deck);
+        PlayerDeckPlayer player = new PlayerDeckPlayer(deck); //oyuncuya kartların verilmesi için
         String [] player_hand=player.giveCardstoPlayer();
 
+        PlayerDeckComputer computer = new PlayerDeckComputer(deck); //bilgisayara kartların verilmesi için
+        String [] computer_hand=computer.giveCardstoComputer();
 
 
 
+        for(String b : computer_hand) {
+            System.out.println(b);
+        }
+        
+        System.out.println();
 
         for(String c : deck) {
             System.out.println(c);
         }
+
         System.out.println();
 
         for(String a : player_hand) {
--- PlayerDeckComputer.java
+++ PlayerDeckComputer.java
@@ -1,2 +1,20 @@
 public class PlayerDeckComputer {
+    public String [] deck;
+    public String [] computer_hand;
+    public PlayerDeckComputer(String[] deck) {
+        this.deck = deck;
+        this.computer_hand = new String[4];
+    }
+
+
+    public String[] giveCardstoComputer(){
+
+        for(int i=0;i < computer_hand.length;i++) {
+            computer_hand[i] = deck[i];
+
+        }
+
+
+        return computer_hand;
+    }
 }
